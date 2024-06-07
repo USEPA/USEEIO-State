449f17953a1f4d2a92b2cf48444b4dd2299c9898
Ben Young <Ben.Young@erg.com>
2024-06-06 20:41:02 -0400

    incoroprate demand by region

--- R/StateEEIOCalculations.R
+++ R/StateEEIOCalculations.R
@@ -24,6 +24,10 @@ calculateStateCBE <- function(model, CO2e=TRUE, perspective="FINAL",
 ## Returns a vector of demand in dollars by type with sectors as rows
 #' @param type, str, "Household", "Federal Government", "State Government", "Investment", "final", or "intermediate"
 getStateUsebyType <- function(model, type="final", domestic=FALSE, RoUS=FALSE) {
+  opt <- c("Household", "Federal Government", "State Government", "Investment", "final", "intermediate")
+  if (!type %in% opt) {
+    stop(paste0("'type' options are ", paste(opt, collapse=", ")))
+  }
   loc <- getLocation(RoUS, model)
   if (type=="final") {
     code_loc <- model$FinalDemandMeta[endsWith(model$FinalDemandMeta$Code_Loc,loc),][["Code_Loc"]]
@@ -48,18 +52,69 @@ getStateUsebyType <- function(model, type="final", domestic=FALSE, RoUS=FALSE) {
 }
 
 
-# Calculate demand by sector by source
-calculateDemandBySource <- function(model) {
-  demand_by_source <- data.frame(sapply(c("Household", "Investment", "Federal Government", "State Government"),
+# Calculate demand by sector by type
+calculateDemandByType <- function(model, price_year=NULL) {
+  demand_by_type <- data.frame(sapply(c("Household", "Investment", "Federal Government", "State Government"),
                                         getStateUsebyType, model=model,
                                         simplify=FALSE, USE.NAMES=FALSE))
-  demand_by_source <- cbind(demand_by_source, Total = rowSums(demand_by_source))
+  demand_by_type <- cbind(demand_by_type, Total = rowSums(demand_by_type))
   
   # if desired, adjust price type before summing
+  if(is.null(price_year)) {
+    price_year <- model$specs$IOYear
+  }
+  rho <- model$Rho[, toString(model$specs$IOYear)] / model$Rho[, toString(price_year)]
+  demand_by_type <- demand_by_type * rho
   
-  total_demand_by_source <- as.matrix(colSums(demand_by_source))
-  colnames(total_demand_by_source) <- "Demand"
-  return(total_demand_by_source)
+  total_demand_by_type <- as.matrix(colSums(demand_by_type))
+  colnames(total_demand_by_type) <- "Demand"
+  return(total_demand_by_type)
+}
+
+getDemandbyRegion <- function(model, region="SoI") {
+  state <- model$specs$ModelRegionAcronyms[[1]]
+  # Get consumption amounts by region
+  # The 3rd demand vector is Consumption complete
+  # The 4th is Consumption domestic. 
+  # The difference should represent total consumption by each
+  
+  soi_comms <- grep(state, names(model$DemandVectors$vectors[[4]]))
+  rous_comms <- grep("RoUS", names(model$DemandVectors$vectors[[4]]))
+  
+  soi_soi_finalconsumption <- model$DemandVectors$vectors[[4]][soi_comms]
+  soi_rous_finalconsumption <- model$DemandVectors$vectors[[4]][rous_comms]
+  
+  soi_import_consumption <- model$DemandVectors$vectors[[3]]-model$DemandVectors$vectors[[4]]
+  
+  soi_import_consumption <- soi_import_consumption[soi_comms]
+  if(region=="SoI") {
+    d <- soi_soi_finalconsumption
+  } else if (region=="RoUS") {
+    d <- soi_rous_finalconsumption
+  } else if (region=="ROW") {
+    d <- soi_import_consumption
+  }
+  d <- as.matrix(d)
+  return(d)
+}
+
+# Calculate demand by sector by region
+calculateDemandByRegion <- function(model, price_year=NULL) {
+  demand_by_region <- data.frame(sapply(c("SoI", "RoUS", "ROW"),
+                                        getDemandbyRegion, model=model,
+                                        simplify=FALSE, USE.NAMES=TRUE))
+  demand_by_region <- cbind(demand_by_region, Total = rowSums(demand_by_region))
+  
+  # if desired, adjust price type before summing
+  if(is.null(price_year)) {
+    price_year <- model$specs$IOYear
+  }
+  rho <- model$Rho[, toString(model$specs$IOYear)] / model$Rho[, toString(price_year)]
+  demand_by_region <- demand_by_region * rho
+  
+  total_demand_by_region <- as.matrix(colSums(demand_by_region))
+  colnames(total_demand_by_region) <- "Demand"
+  return(total_demand_by_region)
 }
 
 
@@ -254,65 +309,3 @@ prepareDFforFigure <- function(model_list, matrix_name=NULL, perspective=NULL,
   return(df)
 }
 
-
-calculateDemandVectors <- function(model, price_year=NULL) {
-  
-  state <- model$specs$ModelRegionAcronyms[[1]]
-  year <- model$specs$IOYear
-  if(is.null(price_year)) {
-    price_year <- toString(year)
-  }
-  
-  # Get consumption amounts by region
-  # The 3rd demand vector is Consumption complete
-  # The 4th is Consumption domestic. 
-  # The difference should represent total consumption by each
-  
-  soi_comms <- grep(state, names(model$DemandVectors$vectors[[4]]))
-  rous_comms <- grep("RoUS", names(model$DemandVectors$vectors[[4]]))
-  
-  soi_soi_finalconsumption <- model$DemandVectors$vectors[[4]][soi_comms]
-  soi_rous_finalconsumption <- model$DemandVectors$vectors[[4]][rous_comms]
-  
-  soi_import_consumption <- model$DemandVectors$vectors[[3]]-model$DemandVectors$vectors[[4]]
-  
-  soi_import_consumption <- soi_import_consumption[soi_comms]
-  
-  # Adjust price years using Rho
-  rho_full <- model$Rho[, toString(year)] / model$Rho[, price_year]
-  rho <- rho_full[soi_comms]
-  soi_consumption <- cbind("In State" = soi_soi_finalconsumption * rho,
-                           "Rest of US" = soi_rous_finalconsumption * rho,
-                           "Imported" = soi_import_consumption * rho)
-  
-  total_demand <- colSums(soi_consumption)
-  total_demand <- data.frame(t(total_demand))
-  total_demand <- cbind(total_demand, Total = rowSums(total_demand))
-  total_demand$Year <- toString(year)
-  
-  # Calculate demand by sector by source
-  # drop value added rows
-  use <- head(model$U, -6)
-  households <- model$FinalDemandMeta[model$FinalDemandMeta$Group == "Household" &
-                                    endsWith(model$FinalDemandMeta$Code_Loc, state), ][["Code_Loc"]]
-  investment <- model$FinalDemandMeta[model$FinalDemandMeta$Group == "Investment" &
-                                    endsWith(model$FinalDemandMeta$Code_Loc, state), ][["Code_Loc"]]
-  state_govt <- model$FinalDemandMeta[(model$FinalDemandMeta$Group == "Government") &
-                                    endsWith(model$FinalDemandMeta$Code_Loc, state) &
-                                    startsWith(model$FinalDemandMeta$Code, "F10"), ][["Code_Loc"]]
-  fed_govt <- model$FinalDemandMeta[(model$FinalDemandMeta$Group == "Government") &
-                                  endsWith(model$FinalDemandMeta$Code_Loc, state)&
-                                  !startsWith(model$FinalDemandMeta$Code, "F10"), ][["Code_Loc"]]
-  demand_by_source <- cbind("Households" = use[, households] * rho_full,
-                            "Investment" = rowSums(use[, investment]) * rho_full,
-                            "State and Local Government" = rowSums(use[, state_govt]) * rho_full,
-                            "Federal Government" = rowSums(use[, fed_govt]) * rho_full
-  )
-  demand_by_source <- cbind(demand_by_source, Total = rowSums(demand_by_source))
-  total_demand_by_source <- colSums(demand_by_source)
-  total_demand_by_source <- data.frame(t(total_demand_by_source))
-  total_demand_by_source$Year <- toString(year)
-  
-  return(list("demand" = total_demand, "demand_by_source" = total_demand_by_source))
-}
-  
